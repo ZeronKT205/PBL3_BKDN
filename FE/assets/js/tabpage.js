6e5af147a34b73011b978f05afeccf1bdd6a71db
ZeronKT205 <tailieusv.desstar1@gmail.com>
2025-03-21 20:28:21 +0700

    First commit for CINEMA_BOOKING

---
+++ FE/assets/js/tabpage.js
@@ -0,0 +1,10 @@
+const pageLogin = "../pages/userlogin.html"; // Biến toàn cục
+const pageSignUp = "../pages/usersignup.html";
+
+
+function goToPageLogin() {
+    window.location.href = pageLogin;
+}
+function goToPageSignUp(){
+    window.location.href = pageSignUp;
+}
