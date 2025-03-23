8164c364dd731f60f2ee1c8ae8d9071c32e762fd
ZeronKT205 <tailieusv.desstar1@gmail.com>
2025-03-24 00:43:22 +0700

    https://github.com/ZeronKT205/PBL3_BKDN.git

--- FE/assets/js/tabpage.js
+++ FE/assets/js/tabpage.js
@@ -1,10 +1,38 @@
-const pageLogin = "../pages/userlogin.html"; // Biến toàn cục
+const pageLogin = "pages/userlogin.html"; // Biến toàn cục
 const pageSignUp = "../pages/usersignup.html";
-
+const pageLogin2 = "../pages/userlogin.html"
 
 function goToPageLogin() {
     window.location.href = pageLogin;
 }
+function goToPageLogin2() {
+    window.location.href = pageLogin2;
+}
 function goToPageSignUp(){
     window.location.href = pageSignUp;
 }
+
+
+function toggleContent(type) {
+    document.querySelectorAll(".description, .trailer, .schedule").forEach(el => el.classList.add("hidden"));
+    document.querySelector("."+type).classList.remove("hidden");
+    
+    document.querySelector(".buttoncontainer__description").classList.remove("active");
+    document.querySelector(".buttoncontainer__trailer").classList.remove("active");
+    document.querySelector(".buttoncontainer__schedule").classList.remove("active");
+    document.querySelector(".buttoncontainer__"+type).classList.add("active");
+
+
+}
+
+// CUỘN TRANG
+
+window.addEventListener("scroll",function(){
+    let navbar = document.querySelector(".navbar");
+    if(this.window.scrollY > 50){
+        navbar.classList.add("scrolled");
+    }
+    else{
+        navbar.classList.remove("scrolled");
+    }
+});
